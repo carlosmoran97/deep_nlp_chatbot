30d813bf2b357a4a5e8bcaaad2e3ef180885ced5
Carlos Morán <carlos@alfredchat.com>
2018-09-15 16:57:05 -0600

    Preprocessing the targets

---
+++ chatbot.py
@@ -0,0 +1,186 @@
+# Building a ChatBot with Deep NLP
+# Carlos Morán
+# 9-sept.-2018
+
+
+# Importing the libraries 
+import numpy as np
+import tensorflow as tf
+import re
+import time
+
+
+
+#### PART I -  DATA PREPROCESSING ####
+
+# Importing the dataset
+lines = open('movie_lines.txt', encoding='utf-8', errors='ignore').read().split('\n')
+conversations = open('movie_conversations.txt', encoding='utf-8', errors='ignore').read().split('\n')
+
+# creating a dictionary that maps each line and its id
+
+id2line = {}
+
+for line in lines:
+    _line = line.split(' +++$+++ ')
+    if len(_line) == 5:
+        id2line[_line[0]] = _line[4]
+
+
+# creating a list of all the conversations
+conversations_ids = []
+
+for conversation in conversations[:-1]:
+    _conversation = conversation.split(' +++$+++ ')[-1][1:-1].replace("'","").replace(" ","")
+    conversations_ids.append(_conversation.split(','))
+
+# getting separately the questions and the answers
+questions = []
+answers = []
+
+for conversation in conversations_ids:
+    for i in range(len(conversation) - 1):
+        questions.append(id2line[conversation[i]])
+        answers.append(id2line[conversation[i+1]])
+    # endfor    
+#endfor
+
+# Doing a first cleaning of the text
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"that's", "that is", text)
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"where's", "where is", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"can't", "can not", text)
+    text = re.sub(r"[-()\"#/@;:<>{}+=~|.?,]", "", text)
+    return text
+# endfunction
+
+# Cleaning the questions
+clean_questions = []    
+for question in questions:
+    clean_questions.append(clean_text(question))
+# Cleaning the answers
+clean_answers = []
+for answer in answers:
+    clean_answers.append(clean_text(answer))
+
+# creating a dictionary that maps each word to its number of ocurrences
+word2count = {}
+for question in clean_questions:
+    for word in question.split():
+        if word not in word2count:
+            word2count[word] = 1
+        else:
+            word2count[word] += 1
+        # endif
+    # endfor
+# endfor
+
+for answer in clean_answers:
+    for word in answer.split():
+        if word not in word2count:
+            word2count[word] = 1
+        else:
+            word2count[word] += 1
+        # endif
+    # endfor
+# endfor
+    
+# creating two dictionaries that map the questions words and the answers to a uniqe integer
+threshold = 20
+questionswords2int = {}
+word_number = 0
+for word, count in word2count.items():
+    if count >= threshold:
+        questionswords2int[word] = word_number
+        word_number += 1
+    # endif
+# endfor
+answerswords2int = {}
+word_number = 0
+for word, count in word2count.items():
+    if count >= threshold:
+        answerswords2int[word] = word_number
+        word_number += 1
+    # endif
+# endfor
+    
+# Adding the last two tokens of these two dictionaries
+tokens = ['<PAD>','<EOS>','<OUT>', '<SOS>']
+for token in tokens:
+    questionswords2int[token] = len(questionswords2int) + 1
+# endfor
+for token in tokens:
+    answerswords2int[token] = len(answerswords2int) + 1
+
+
+# creating the inverse dictionary of the answerswords2int dictionary
+answersints2word = {w_i: w for w,w_i in answerswords2int.items()}
+
+# Adding the EOS token in every anwers
+
+for i in range(len(clean_answers)):
+    clean_answers[i] += ' <EOS>'
+
+questions_into_int = []
+for question in clean_questions:
+    ints = []
+    for word in question.split():
+        if word not in questionswords2int:
+            ints.append(questionswords2int['<OUT>'])
+        else:
+            ints.append(questionswords2int[word])
+        #endif
+    questions_into_int.append(ints)
+    #endfor
+#endfor
+answers_into_int = []
+for answer in clean_answers:
+    ints = []
+    for word in answer.split():
+        if word not in answerswords2int:
+            ints.append(answerswords2int['<OUT>'])
+        else:
+            ints.append(answerswords2int[word])
+        #endif
+    answers_into_int.append(ints)
+    #endfor
+#endfor
+# Sorting questions and answers by the length of questions
+sorted_clean_questions = []
+sorted_clean_answers = []
+
+for length in range(1, 25 + 1):
+    for i in enumerate(questions_into_int):
+        if len(i[1]) == length:
+            sorted_clean_questions.append(questions_into_int[i[0]])
+            sorted_clean_answers.append(answers_into_int[i[0]])
+        # end if
+    # endfor
+# endfor
+
+###  PART II - BUILDING THE SEQ2SEQ MODEL   ###
+
+# Creating placeholders for the inputs and the targets
+def model_inputs():
+    inputs = tf.placeholder(tf.int32, [None, None], name='input')
+    targets = tf.placeholder(tf.int32, [None, None], name='target')
+    lr = tf.placeholder(tf.float32, name='learning_rate')
+    keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+    return inputs, targets, lr, keep_prob
+
+# Preoprocessing the targets
+def preprocess_targets(targets, word2int, batch_size):
+    left_side = tf.fill([batch_size, 1], word2int['<SOS>'])
+    right_side = tf.strided_slice(targets, [0,0], [batch_size, -1], [1,1])
+    preprocessed_targets = tf.concat([left_side, right_side], 1)
+    return preprocessed_targets
+
